fa1bfb32fa69bd72aa74cc428649793db9e1e0ff
kestas <red.reduose@gmail.com>
2024-05-27 22:41:23 +0300

    add on folder context menu too

--- src-tauri/src/custom.rs
+++ src-tauri/src/custom.rs
@@ -6,27 +6,43 @@ use winreg::{
 #[tauri::command]
 pub fn reg_right(path: &str) -> Result<(), String> {
     let hcr = RegKey::predef(HKEY_CLASSES_ROOT);
-    // 文件夹内空白区域右键注册
+    let hcu = RegKey::predef(HKEY_CURRENT_USER);
+
+    // Register right-click menu for empty area in a folder
     let (reg_name, _) = hcr
         .create_subkey("Directory\\Background\\shell\\Zed")
         .map_err(|e| format!("{:?}", e))?;
-    // 注册icon图标
     reg_name
         .set_value("Icon", &path)
         .map_err(|e| format!("{:?}", e))?;
-    // 注册名称
     reg_name
         .set_value("", &"Open in Zed")
         .map_err(|e| format!("{:?}", e))?;
-    // command reg
     let (reg_comm, _) = hcr
         .create_subkey("Directory\\Background\\shell\\Zed\\command")
         .map_err(|e| format!("{:?}", e))?;
     reg_comm
         .set_value("", &format!("{} \"%V\"", path))
         .map_err(|e| format!("{:?}", e))?;
-    // 注册文件右键
-    let hcu = RegKey::predef(HKEY_CURRENT_USER);
+
+    // Register right-click menu for folders
+    let (reg_folder_name, _) = hcu
+        .create_subkey("SOFTWARE\\Classes\\Directory\\shell\\Zed")
+        .map_err(|e| format!("{:?}", e))?;
+    reg_folder_name
+        .set_value("Icon", &path)
+        .map_err(|e| format!("{:?}", e))?;
+    reg_folder_name
+        .set_value("", &"Open in Zed")
+        .map_err(|e| format!("{:?}", e))?;
+    let (reg_folder_comm, _) = hcu
+        .create_subkey("SOFTWARE\\Classes\\Directory\\shell\\Zed\\command")
+        .map_err(|e| format!("{:?}", e))?;
+    reg_folder_comm
+        .set_value("", &format!("{} \"%V\"", path))
+        .map_err(|e| format!("{:?}", e))?;
+
+    // Register right-click menu for files
     let (reg_file_name, _) = hcu
         .create_subkey("SOFTWARE\\Classes\\*\\shell\\Zed")
         .map_err(|e| format!("{:?}", e))?;
@@ -47,14 +63,22 @@ pub fn reg_right(path: &str) -> Result<(), String> {
 
 #[tauri::command]
 pub fn delete_right() -> Result<(), String> {
-    // 删除文件夹右键
     let hcr = RegKey::predef(HKEY_CLASSES_ROOT);
+    let hcu = RegKey::predef(HKEY_CURRENT_USER);
+
+    // Delete folder right-click menu
     hcr.delete_subkey(r"Directory\Background\shell\Zed\command")
         .map_err(|e| format!("{:?}", e))?;
     hcr.delete_subkey(r"Directory\Background\shell\Zed")
         .map_err(|e| format!("{:?}", e))?;
-    // 删除文件右键
-    let hcu = RegKey::predef(HKEY_CURRENT_USER);
+
+    // Delete folder right-click menu
+    hcu.delete_subkey(r"SOFTWARE\Classes\Directory\shell\Zed\command")
+        .map_err(|e| format!("{:?}", e))?;
+    hcu.delete_subkey(r"SOFTWARE\Classes\Directory\shell\Zed")
+        .map_err(|e| format!("{:?}", e))?;
+
+    // Delete file right-click menu
     hcu.delete_subkey(r"SOFTWARE\Classes\*\shell\Zed\command")
         .map_err(|e| format!("{:?}", e))?;
     hcu.delete_subkey(r"SOFTWARE\Classes\*\shell\Zed")
